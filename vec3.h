d2b1db932aacaedbea9ea04b5e8fb01b46c2a7e9
Richard-Qin-001 <richard.qin.001@gmail.com>
2026-03-09 02:21:09 +0800

    Create class vec3

---
+++ vec3.h
@@ -0,0 +1,41 @@
+#ifndef VEC_3
+#define VEC_3
+
+#include <cmath>
+#include <iostream>
+
+class vec3{
+  public:
+    double e[3];
+
+    vec3() : e{0, 0, 0} {}
+    vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+
+    double x() const;
+    double y() const;
+    double z() const;
+
+    vec3 operator-() const;
+    double operator[](int i) const;
+    double &operator[](int i);
+    vec3& operator+=(const vec3 & other);
+    vec3& operator*=(double t);
+    vec3& operator/=(double t);
+    double length() const;
+    double length_squared() const;
+
+
+};
+
+inline std::ostream &operator<<(std::ostream &out, const vec3 &v);
+inline vec3 operator+(const vec3 &u, const vec3 &v);
+inline vec3 operator-(const vec3 &u, const vec3 &v);
+inline vec3 operator*(const vec3 &u, const vec3 &v);
+inline vec3 operator*(double t, const vec3 &v);
+inline vec3 operator*(const vec3 &v, double t);
+inline vec3 operator/(const vec3 &v, double t);
+inline double dot(const vec3 &u, const vec3 &v);
+inline vec3 cross(const vec3 &u, const vec3 &v);
+inline vec3 unit_vector(const vec3 &v);
+
+#endif
